1401c2beec3be34ac74b71247f58c6c4fbe10ed5
adith-nr <adithreganti@gmail.com>
2025-01-19 23:22:20 +0530

    Update model.py

--- model.py
+++ model.py
@@ -24,12 +24,9 @@ X[:,7:]=imputer.transform(X[:,7:])
 
 X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=42)
 
-# sc = StandardScaler()
-# sc2 = StandardScaler()
-# X_train[:,7:] = sc.fit_transform(X_train[:,7:])
-# X_test[:,7:] = sc.transform(X_test[:,7:])
+
 y_train =y_train.reshape(len(y_train),1)
-# y_train=sc2.fit_transform(y_train)
+
 
 
 
@@ -43,8 +40,7 @@ import joblib
 # Save model and preprocessing objects
 joblib.dump(regressor, 'model.pkl')  # Save trained model
 joblib.dump(poly_reg, 'poly_transform.pkl')  # Save polynomial features transformer
-# joblib.dump(sc, 'scaler.pkl')  # Save input feature scaler
-# joblib.dump(sc2, 'scaler_y.pkl')  # Save target variable scaler
+
 joblib.dump(ct, 'column_transform.pkl')  # Save column transformer
 
 print("Model and preprocessors saved successfully!")
