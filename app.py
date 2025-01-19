ac414127f468770a86bff57cce156a46f0c13116
adith-nr <adithreganti@gmail.com>
2025-01-19 23:18:16 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask,request,jsonify
+from flask_cors import CORS
+import numpy as np
+import joblib
+
+app = Flask(__name__)
+CORS(app)
+model = joblib.load('model.pkl')
+poly_reg = joblib.load('poly_transform.pkl')
+sc = joblib.load('scaler.pkl')
+sc2 = joblib.load('scaler_y.pkl')
+ct = joblib.load('column_transform.pkl')
+
+@app.route('/predict',methods=['POST'])
+
+def predict():
+    try:
+        
+        data = request.get_json()
+        
+        print(data['features'])
+        
+        # features_transformed = ct.transform([data['features']])
+        features_transformed=np.array([data['features']])
+        features_transformed[:, 7:] = sc.transform(features_transformed[:, 7:])
+        features_poly = poly_reg.fit_transform(features_transformed)
+
+
+        prediction = model.predict(features_poly)
+
+        prediction = sc2.inverse_transform(prediction)
+
+        return jsonify({'prediction':prediction.tolist()})
+    except Exception as e:
+        return jsonify({'error':str(e)})
+    
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ model.py
@@ -0,0 +1,50 @@
+import numpy as np
+import pandas as pd
+from flask import Flask,request,jsonify
+from sklearn.impute import SimpleImputer
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import StandardScaler 
+df = pd.read_csv('Fish.csv')
+
+
+X = df.drop(columns='Weight').values
+y = df['Weight'].values
+
+ct = ColumnTransformer(transformers=[('encode',OneHotEncoder(),[0])],remainder='passthrough')
+X = np.array(ct.fit_transform(X))
+
+imputer = SimpleImputer(strategy='mean')
+imputer.fit(X[:,7:])
+X[:,7:]=imputer.transform(X[:,7:])
+
+
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=42)
+
+# sc = StandardScaler()
+# sc2 = StandardScaler()
+# X_train[:,7:] = sc.fit_transform(X_train[:,7:])
+# X_test[:,7:] = sc.transform(X_test[:,7:])
+y_train =y_train.reshape(len(y_train),1)
+# y_train=sc2.fit_transform(y_train)
+
+
+
+poly_reg= PolynomialFeatures(degree=2)
+X_poly_train = poly_reg.fit_transform(X_train)
+regressor = LinearRegression()
+regressor.fit(X_poly_train,y_train)
+
+import joblib
+
+# Save model and preprocessing objects
+joblib.dump(regressor, 'model.pkl')  # Save trained model
+joblib.dump(poly_reg, 'poly_transform.pkl')  # Save polynomial features transformer
+# joblib.dump(sc, 'scaler.pkl')  # Save input feature scaler
+# joblib.dump(sc2, 'scaler_y.pkl')  # Save target variable scaler
+joblib.dump(ct, 'column_transform.pkl')  # Save column transformer
+
+print("Model and preprocessors saved successfully!")
